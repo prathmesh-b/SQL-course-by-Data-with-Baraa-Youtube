869c55f2041704ae588c6effdb11bbe705470281
Prathmesh <rafalemultirole94@gmail.com>
2025-09-28 12:57:20 +0530

    new files

--- LVL 2 - INTERMEDIATE SQL/Part 5 - Combining Data/Joins Basic/tut23 Types of Joins.sql
+++ LVL 2 - INTERMEDIATE SQL/Part 5 - Combining Data/Joins Basic/tut23 Types of Joins.sql
@@ -43,7 +43,14 @@ FROM orders;
 
 -- output show two complete table of customers and orders
 
-2) INNER JOIN
+
+
+
+
+
+
+------- 2) INNER JOIN --------
+
 Returns only matching Rows from both Tables
 
 common data (matching data) from both table
@@ -78,3 +85,254 @@ ON A.Key = B.Key
 == TASK ==
 Get all customers along with their orders, but only
 for customers who have placed an order
+
+== code ==
+SELECT *
+FROM customers
+INNER JOIN orders
+ON id = customer_id
+
+we don't get no 4,5 data because there is not matching id in both table
+
+we got required output but we see id , customer_id again repeating
+like 1,2,3
+
+so we will change code and select required column only
+== updated code==
+
+SELECT 
+	id,
+	first_name,
+	order_id,
+	sales
+FROM customers
+INNER JOIN orders
+ON id = customer_id
+
+
+=== impt issue == Column Ambiguity(sometimes u set same name columns)
+solution== Add the table name before the column to avoid confusion
+in joins with same-named columns
+
+you can write customers.id which means id of customers table
+orders.id which means id of orders column
+
+above code be like
+SELECT 
+	customers.id, (id from customer table)
+	first_name,
+	orders.id,    (orders is table name. which means id column from orders table)
+	sales
+FROM customers
+INNER JOIN orders
+ON customers.id = orders.customer_id
+
+
+== TIPS ===== in order to save time customers.id also can be written to save time
+c.id (sql understand u telling customers table id column)
+====
+
+=== uPdated Code ==
+
+SELECT 
+	c.id,
+	c.first_name,
+	o.order_id,
+	o.sales
+FROM customers AS c
+INNER JOIN orders AS o
+ON c.id = o.customer_id
+-- when u doing inner joins of two tables order not matter like 
+-- FROM customers AS c INNER JOIN orders AS o , or like FROM orders AS o INNER JOIN customer AS c
+
+== how inner join works ==
+it start matching row from 1 table FROM__ with other table, row which matches are then displayed in result
+
+
+====== INNER JOIN when it will be use ======
+1) Recombine Data from tables -- "Big Picture" to get
+2) Check Existence -- "Filtering" the data
+
+
+
+
+
+
+------- 3) LEFT JOIN --------
+
+Returns all rows from left table (A) and only matching one from right table (B)
+
+-- impt-- 
+left table A is Primary source of data we can't miss anything from it
+right table B is secondary source of data we use just to get additional data
+
+
+===== Syntax ======
+Here Order of Table is very important 
+
+SELECT *
+FROM A -- Left table data from here everything
+LEFT JOIN B  -- Right table
+ON A.Key = B.Key
+
+====================
+
+=== TASK ===
+Get all customers along with their orders, including
+those without orders
+
+SELECT 
+	c.id,
+	c.first_name,
+	o.order_id,
+	o.sales
+FROM customers AS c   -- here customer is left table so FROM 
+LEFT JOIN orders AS o
+ON c.id = o.customer_id
+
+--null means no matching data
+
+
+
+====== LEFT JOIN when it will be use ======
+1) Recombine Data from tables -- "Big Picture" to get   (INNER LEFT)
+2) DATA Enrichment "Extra Info" (LEFT)
+3) Check Existence -- "Filtering" the data    (INNER LEFT + WHERE)
+
+
+
+
+
+
+
+
+------- 4) RIGHT JOIN --------
+
+This is total opposite of LEFT JOIN
+
+Returns all rows from Right table (B) and only matching one from Left table (A)
+
+-- impt-- 
+left table A is Secondary source of data we use just to get additional data            
+right table B is Primary source of data we get all data from can't miss anything from it
+
+
+===== Syntax ======
+Here Order of Tables is very important 
+
+SELECT *
+FROM A -- Left table 
+RIGHT JOIN B  -- Right table data from here everything
+ON A.Key = B.Key
+
+====================
+
+=== TASK ===
+
+Get all customers along with their orders including orders without
+matching customers
+
+SELECT 
+	c.id,
+	c.first_name,
+	o.order_id,
+	o.sales
+FROM customers AS c   
+RIGHT JOIN orders AS o  -- here order table is right we want all data from it
+ON c.id = o.customer_id
+
+here despit * not used with SELECT to get all data from 
+orders table RIGHT JOIN gives all data from right table
+
+--null means no matching data
+
+
+
+
+======= SOLVE SAME TASK USING LEFT JOIN =======
+solution below
+
+
+
+
+====== ALternative to RIGHT JOIN ======
+ we can switch table A and B to B=left A=right
+
+
+
+
+ A= LEFT TABLE B= RIGHT TABLE           B= Left table A=Right Table
+
+ SELECT *                                  SELECT *  
+ FROM A                                    FROM B
+ RIGHT JOIN B            ALternative       LEFT JOIN A 
+ ON A.Key = B.Key                          ON A.Key = B.Key    
+
+
+ WE  GET   SAME RESULT 
+
+=== solution for SOLVE SAME TASK USING LEFT JOIN ======
+SELECT 
+	c.id,
+	c.first_name,
+	o.order_id,
+	o.sales
+FROM orders AS o   
+LEFT JOIN customers AS c  
+ON c.id = o.customer_id
+
+
+
+ LEFT JOIN  IS MOST USED
+ RIGHT JOIN NOT SO FAMOUS
+
+ so advice of mentor is to stick with LEFT JOIN and leave RIGHT 
+ Just have order of table right
+
+
+
+
+
+
+ ------- 5) FULL JOIN --------
+
+ Returns all the rows from both the tables
+
+ A table all rows
+ B table all rows
+
+
+ The order of tables doesn't matter
+
+ SELECT *
+ FROM A
+ FULL JOIN B
+ ON A.Key = B.Key
+
+ OR
+
+
+ SELECT *
+ FROM B
+ FULL JOIN A
+ ON A.Key = B.Key
+
+
+ ====== Task =====
+ Get all customers and all orders,
+ even if there's no match
+
+ SELECT 
+	c.id,
+	c.first_name,
+	o.order_id,
+	o.sales
+FROM customers AS c   
+FULL JOIN orders AS o
+ON c.id = o.customer_id
+
+
+ even if you  change the table orders u get same data 
+
+ === use case ===
+ check existence "filetering"  FULL + WHERE 
---
+++ LVL 2 - INTERMEDIATE SQL/Part 5 - Combining Data/Joins Basic/tut24 Advance SQL Joins.sql
@@ -0,0 +1,143 @@
+/*
+tut 24 ADVANCE SQL JOINS: ANTI & CROSS
+
+ANTI JOIN - Finding everything that is in one table and not in the other
+
+some business question we can answers like
+- What customers have NOT placed an order?
+- What products do NOT have inventory?
+
+Advance joins type
+
+
+
+=== 1) LEFT ANTI JOIN ===
+
+Returns Row from Left that has NO MATCH in Right
+
+== A Table ==
+Only UnMatching Rows
+
+Primary source {main source}
+
+== B Table ==
+Nothing 
+
+Lookup (Filter)
+Not for Data, Just a Filter
+
+==== Syntax ====
+the order of tables is important
+
+SELECT *
+FROM A
+LEFT JOIN B
+ON A.Key = B.Key
+WHERE B.Key IS NULL  -- WHERE for lookup (filter) and B Table has disabled to just lookup
+
+
+
+===== TASK =====
+Get all customers who haven't placed any order
+
+
+SELECT *
+FROM customers AS c
+LEFT JOIN orders AS o
+ON c.id = o.customer_id
+WHERE o.customer_id IS NULL  -- here we want customers id who have not order anything
+
+how to code STEP 1 do classical select and left join
+then apply WHERE Step 2 for filtering
+
+=== USECASE ===
+we want to check existence "filtering"
+
+
+
+=== 2) RIGHT ANTI JOIN ===
+
+it's opposite of LEFT ANTI JOIN 
+
+Returns Row from Right that has NO MATCH in Left
+
+== B Table ==
+Only UnMatching Rows
+
+Primary source {main source}
+
+== A Table ==
+Nothing 
+
+Lookup (Filter)
+Not for Data, Just a Filter
+
+
+==== Syntax ====
+the order of tables is important
+
+SELECT *
+FROM A
+RIGHT JOIN B
+ON A.Key = B.Key
+WHERE A.Key IS NULL  -- WHERE for lookup (filter) and A Table has disabled to just lookup
+
+
+=== TASK ===
+Get all orders without matching customers
+
+SELECT *
+FROM customers AS c
+RIGHT JOIN orders AS o
+ON c.id = o.customer_id
+WHERE c.id  IS NULL
+
+ALTERNATIVE Solution with LEFT JOIN
+
+switch the table as done earlier
+
+SELECT *
+FROM orders AS o
+LEFT JOIN customers AS c
+ON c.id = o.customer_id
+WHERE c.id  IS NULL
+
+
+u get same exact result
+
+
+
+
+==== 3) FULL ANTI JOIN ====
+Returns only rows that don't match in either Tables
+
+
+ONLY Unmatching data (rows)
+
+
+==== Syntax ====
+the order of tables doesn't matter
+
+
+
+SELECT *
+FROM A
+FULL JOIN B
+ON A.Key = B.Key
+WHERE
+B.Key IS NULL
+	OR
+A.Key IS NULL
+
+
+= == Task ===
+Find customers without orders and order without customers
+
+
+SELECT *
+FROM orders AS o
+FULL JOIN customers AS c
+ON c.id = o.customer_id
+WHERE c.id IS NULL OR o.customer_id IS NULL
+
+-- continue from 13:21
